a5b78a89d1112a743955ed6fb83c9437cbc5e45b
KubaNocon7890 <42960635+KonneckY@users.noreply.github.com>
2021-03-09 00:03:15 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SoyOla</title>
+    <link rel="stylesheet" href="style-mainsite.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <div class="icons-div">
+                <a href="#"><img src="ikona-koszyk.png" alt="" class="icon"></a>
+                <a href="#"><img src="ikona-user.png" alt="" class="icon"></a>
+            </div>
+            <div class="titlecont">
+            <h1 class="title">SOYOLA</h1>
+            <h3 class="title2">Hand made with ♥ by Ola.</h3>         
+        </div>
+        <div class="color-break"></div>
+        <div class="nav">
+            <input type="checkbox">
+              <span></span>
+              <span></span>
+              <div class="menu">
+                <li><a href="#">O mnie</a></li>
+                <li><a href="#">Świece</a></li>
+                <li><a href="https://www.instagram.com/swieca_sojowa_soyola/">Instagram</a></li>
+                <li><a href="#">Kontakt</a></li>
+              </div>
+          </div>
+        </header>
+        <main>
+            <ul class="slideshow">
+                <li><span></span></li>
+              <li><span></span></li>
+                <li><span></span></li>
+                <li><span></span></li>
+                <li><span></span></li>
+            </ul>
+        </main>
+    </div>
+</body>
+</html>
